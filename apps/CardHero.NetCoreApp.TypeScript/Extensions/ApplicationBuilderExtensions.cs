21e673e5fa30230adc252f74bcdc7bf03fdb72c7
kwokkan <kwokkan@users.noreply.github.com>
2019-09-06 20:22:19 +0100

    Return JSON when part of the api throws an exception.
    (cherry picked from commit cbd255f0b602a2165b01d25f551aab980eb8e129)

---
+++ apps/CardHero.NetCoreApp.TypeScript/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using CardHero.NetCoreApp.TypeScript.MiddleWare;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseJsonException(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<JsonExceptionMiddleware>();
+
+            return app;
+        }
+    }
+}
