a84a456feb17dc78af06bbb9781c353dee3c69d5
kwokkan <kwokkan@users.noreply.github.com>
2019-09-10 20:28:39 +0100

    Feature/11 json errors (#12)
    * Return JSON when part of the api throws an exception.
    
    (cherry picked from commit cbd255f0b602a2165b01d25f551aab980eb8e129)
    
    * Fix middleware namespace.
    
    (cherry picked from commit a83798481444b714843a26f1d1d2d3918600a37a)
    
    * Folder name change.
    
    (cherry picked from commit d962c04377e093acacc268510147502938562d54)
    
    * Folder name change back.
    
    (cherry picked from commit 4a3bb986d3086efe2c40cf551a639e11f97356ef)

---
+++ apps/CardHero.NetCoreApp.TypeScript/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using CardHero.NetCoreApp.TypeScript.Middleware;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseJsonException(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<JsonExceptionMiddleware>();
+
+            return app;
+        }
+    }
+}
---
+++ apps/CardHero.NetCoreApp.TypeScript/Middleware/JsonExceptionMiddleware.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using CardHero.NetCoreApp.TypeScript.Models;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace CardHero.NetCoreApp.TypeScript.Middleware
+{
+    public class JsonExceptionMiddleware
+    {
+        private static readonly string _newLine = Environment.NewLine;
+        private static readonly JsonSerializerSettings _settings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            NullValueHandling = NullValueHandling.Ignore,
+        };
+
+        private readonly ILogger<JsonExceptionMiddleware> _logger;
+        private readonly IHostingEnvironment _environment;
+        private readonly RequestDelegate _next;
+
+        public JsonExceptionMiddleware(ILogger<JsonExceptionMiddleware> logger, IHostingEnvironment environment, RequestDelegate next)
+        {
+            _logger = logger;
+            _environment = environment;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception e)
+            {
+                if (context.Response.HasStarted)
+                {
+                    _logger.LogWarning("Skipping " + nameof(JsonExceptionMiddleware) + ". Response has alreaady started.");
+                    throw;
+                }
+
+                _logger.LogError(e, $"Exception occurred at { context.Request.Method } { context.Request.Path }.");
+
+                if (context.Request.Path.StartsWithSegments("/api"))
+                {
+                    context.Response.StatusCode = 400;
+                    context.Response.ContentType = "application/json";
+
+                    var model = new ErrorViewModel
+                    {
+                        Message = e.Message,
+                    };
+
+                    if (_environment.IsDevelopment())
+                    {
+                        model.StackTrace = e.StackTrace.Split(_newLine);
+                    }
+
+                    var response = JsonConvert.SerializeObject(model, _settings);
+
+                    using (var ms = new MemoryStream())
+                    using (var sw = new StreamWriter(ms))
+                    {
+                        await sw.WriteAsync(response);
+                        await sw.FlushAsync();
+                        ms.Position = 0;
+
+                        await context.Response.Body.WriteAsync(ms.ToArray());
+                    }
+                }
+                else
+                {
+                    context.Response.StatusCode = 500;
+                }
+            }
+        }
+    }
+}
