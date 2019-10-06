a67ec7c875d4cd74c4e640e8640b12b883518010
kwokkan <kwokkan@users.noreply.github.com>
2019-10-06 18:18:05 +0100

    Bug/16 error view (#70)
    * #16: Display a nice page when a render exception occurs.
    
    This also lets users create a new issue.
    
    * Enable compression for files to reduce the load of the ever growing vendor js.

---
+++ apps/CardHero.NetCoreApp.TypeScript/ClientApp/components/shared/ErrorBoundary.tsx
@@ -0,0 +1,45 @@
+﻿import React, { Component, ErrorInfo } from "react";
+import { ErrorView } from "./ErrorView";
+
+interface IErrorBoundatyState {
+    hasError: boolean;
+    error?: Error;
+    errorInfo?: ErrorInfo;
+}
+
+export class ErrorBoundary extends Component<any, IErrorBoundatyState> {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hasError: false
+        };
+    }
+
+    static getDerivedStateFromError(error): IErrorBoundatyState {
+        const newState: IErrorBoundatyState = {
+            hasError: true,
+            error: error
+        };
+
+        if (Constants.Debug) {
+            console.error(error);
+        }
+
+        return newState;
+    }
+
+    componentDidCatch(error, errorInfo) {
+        if (Constants.Debug) {
+            console.error(error, errorInfo);
+        }
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <ErrorView error={this.state.error} errorInfo={this.state.errorInfo} />;
+        }
+
+        return this.props.children;
+    }
+}
--- apps/CardHero.NetCoreApp.TypeScript/ClientApp/styles/index.tsx
+++ apps/CardHero.NetCoreApp.TypeScript/ClientApp/styles/index.tsx
@@ -1,17 +1,23 @@
 ﻿import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCode, faCoins, faPlus, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faBug, faCode, faCoins, faPlus, faStar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
+// redefine prefixes so that the Icon function is easier to use
+faGithub.prefix = "fas";
+
 library.add(
+    faBug,
     faCode,
     faCoins,
+    faGithub,
     faPlus,
     faStar
 );
 
 interface IIconProps {
-    icon: "code" | "coins" | "plus" | "star";
+    icon: "bug" | "code" | "coins" | "github" | "plus" | "star";
     className?: string;
 }
 
