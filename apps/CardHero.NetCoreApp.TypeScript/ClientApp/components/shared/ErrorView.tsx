8d55c7a82ea3fe6d17f25a67a3c824dfa7bb8311
kwokkan <kwokkan@users.noreply.github.com>
2019-10-06 18:16:53 +0100

    #16: Display a nice page when a render exception occurs.
    This also lets users create a new issue.

---
+++ apps/CardHero.NetCoreApp.TypeScript/ClientApp/components/shared/ErrorView.tsx
@@ -0,0 +1,65 @@
+﻿import React, { ErrorInfo, PureComponent } from "react";
+import { Rarity } from "../../clients/clients";
+import Icon from "../../styles/index";
+
+interface IErrorViewProps {
+    error: Error;
+    errorInfo?: ErrorInfo;
+}
+
+export class ErrorView extends PureComponent<IErrorViewProps, any>{
+    render() {
+        const e = this.props.error;
+        const ei = this.props.errorInfo;
+
+        const newIssueUrl = new URL(Constants.NewIssueUrl);
+        newIssueUrl.searchParams.append('title', e.name + ': ' + e.message);
+        newIssueUrl.searchParams.append('body', e.stack);
+        newIssueUrl.searchParams.append('labels', 'bug');
+
+        return (
+            <div className="container-fluid body-content">
+                <div className="row">
+                    <div className="card mx-auto">
+                        <h4 className="card-header">
+                            Oops
+                        </h4>
+                        <div className="card-body">
+                            <p className="card-text">
+                                Looks like some bugs have managed to creep in.
+                            </p>
+
+                            <p className="card-text text-center">
+                                {Array(Rarity.Legendary).fill(0).map((_, i) =>
+                                    <Icon key={i} icon="bug" className={'text-rarity_' + Rarity[i + 1].toLowerCase()} />
+                                )}
+                            </p>
+
+                            <div className="card-text text-muted">
+                                <details>
+                                    <summary >{e.name}: {e.message}</summary>
+                                    <pre id="stacktrace">
+                                        {e.stack}
+                                    </pre>
+                                </details>
+                            </div>
+
+                            {ei &&
+                                <p className="card-text text-muted">
+                                    {ei.componentStack}
+                                </p>
+                            }
+                        </div>
+                        <div className="card-footer">
+                            <a href={newIssueUrl.toString()} className="btn btn-primary pull-right" target="_blank">
+                                <Icon icon="github" />
+                                {' '}
+                                Report an issue
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
