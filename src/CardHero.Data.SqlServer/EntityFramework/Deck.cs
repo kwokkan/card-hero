5155171c5f3e896eecc5245894fd4656e64c1d1d
kwokkan <kwokkan@users.noreply.github.com>
2019-10-10 23:07:49 +0100

    #5: Add auto generated EF data entities.

---
+++ src/CardHero.Data.SqlServer/EntityFramework/Deck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CardHero.Data.SqlServer.EntityFramework
+{
+    public partial class Deck
+    {
+        public Deck()
+        {
+            DeckCardCollection = new HashSet<DeckCardCollection>();
+        }
+
+        public int DeckPk { get; set; }
+        public byte[] Rowstamp { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime? UpdatedTime { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int MaxCards { get; set; }
+        public int UserFk { get; set; }
+
+        public virtual User UserFkNavigation { get; set; }
+        public virtual ICollection<DeckCardCollection> DeckCardCollection { get; set; }
+    }
+}
