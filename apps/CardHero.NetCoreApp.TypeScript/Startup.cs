4bade4b79bc0bc765e1cdd5f2e7312871d4cf67c
kwokkan <kwokkan@users.noreply.github.com>
2019-10-13 20:01:04 +0100

    Disable minification of Swagger pages so that they can be served again. (#90)

--- apps/CardHero.NetCoreApp.TypeScript/Startup.cs
+++ apps/CardHero.NetCoreApp.TypeScript/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using WebMarkupMin.AspNet.Common.UrlMatchers;
 using WebMarkupMin.AspNetCore3;
 
 namespace CardHero.NetCoreApp.TypeScript
@@ -113,6 +114,8 @@ namespace CardHero.NetCoreApp.TypeScript
                     x.MinificationSettings.WhitespaceMinificationMode = WebMarkupMin.Core.WhitespaceMinificationMode.Aggressive;
 
                     x.SupportedHttpMethods.Add("POST");
+
+                    x.ExcludedPages.Add(new WildcardUrlMatcher("/swagger/*"));
                 })
             ;
 
