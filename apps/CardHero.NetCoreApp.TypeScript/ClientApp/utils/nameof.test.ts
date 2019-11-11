ff7cb67e2b85eccfca6857ad3fd460ed43d87f36
kwokkan <kwokkan@users.noreply.github.com>
2019-11-11 20:45:30 +0000

    Feature/113 react eject (#160)
    * #113: Eject react app so tests can be ran.
    
    * #113: Add a test to test the test script.
    
    Also fix a create react app bug: https://github.com/facebook/create-react-app/issues/6099.
    
    * #113: Add jest types for intellisense.

---
+++ apps/CardHero.NetCoreApp.TypeScript/ClientApp/utils/nameof.test.ts
@@ -0,0 +1,9 @@
+﻿import { nameof } from "./nameof";
+
+interface IMockNameof {
+    name: string;
+}
+
+test("nameof return correct information", () => {
+    expect(nameof<IMockNameof>("name")).toBe("name");
+});
