ea679a9c8c7a88bb65a10abfaca04a1c9a28cd0a
kwokkan <kwokkan@users.noreply.github.com>
2019-11-23 16:20:00 +0000

    Add code coverage to js tests.

---
+++ apps/CardHero.NetCoreApp.TypeScript/ClientApp/utils/date.test.ts
@@ -0,0 +1,7 @@
+﻿import { relativeFromNow } from "./date";
+
+test("Current date is a few seconds ago", () => {
+    const df = relativeFromNow(new Date());
+
+    expect(df).toBe("a few seconds ago");
+});
