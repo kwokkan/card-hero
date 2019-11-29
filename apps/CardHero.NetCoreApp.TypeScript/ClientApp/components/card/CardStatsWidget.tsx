7e26e7ebc43ccbc7dadf4346c5ef8c93869b2953
kwokkan <kwokkan@users.noreply.github.com>
2019-11-29 22:30:52 +0000

    #228: Move existing stats into new component.

---
+++ apps/CardHero.NetCoreApp.TypeScript/ClientApp/components/card/CardStatsWidget.tsx
@@ -0,0 +1,69 @@
+﻿import React, { Component } from "react";
+import { ICardModel } from "../../clients/clients";
+
+interface ICardStatsWidgetProps {
+    card: ICardModel;
+}
+
+export class CardStatsWidget extends Component<ICardStatsWidgetProps, {}> {
+    render() {
+        const c = this.props.card;
+
+        return (
+            <div className="card">
+                <h4 className="card-header">
+                    {c.name}
+                </h4>
+                <div className="card-body">
+                    <table className="table table-striped">
+                        <thead className="thead-inverse">
+                            <tr>
+                                <th colSpan={2}>Stats</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">Health</th>
+                                <td>{c.health}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Attack</th>
+                                <td>{c.attack}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Defence</th>
+                                <td>{c.defence}</td>
+                            </tr>
+                            <tr>
+                                <td colSpan={2}></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Up Attack</th>
+                                <td>{c.upAttack}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Right Attack</th>
+                                <td>{c.rightAttack}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Down Attack</th>
+                                <td>{c.downAttack}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Left Attack</th>
+                                <td>{c.leftAttack}</td>
+                            </tr>
+                            <tr>
+                                <td colSpan={2}></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Total Stats</th>
+                                <td>{c.totalStats}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
+}
