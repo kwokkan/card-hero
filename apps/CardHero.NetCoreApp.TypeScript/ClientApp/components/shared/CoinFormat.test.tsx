6c2f6ac48d3307b210f248e0b3977a329e81a1f6
kwokkan <kwokkan@users.noreply.github.com>
2019-11-29 20:47:20 +0000

    #229: Fix issue with stripping empty still returning 0.

---
+++ apps/CardHero.NetCoreApp.TypeScript/ClientApp/components/shared/CoinFormat.test.tsx
@@ -0,0 +1,28 @@
+﻿import React from "react";
+import renderer from "react-test-renderer";
+import { CoinFormat } from "./CoinFormat";
+
+test("<CoinFormat /> renders all 123 gold, 45 silver, 67 bronze", () => {
+    const tree = renderer
+        .create(<CoinFormat coins={1234567} />)
+        .toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
+
+test("<CoinFormat /> renders with 40 silver with others as 0", () => {
+    const tree = renderer
+        .create(<CoinFormat coins={4000} />)
+        .toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
+
+
+test("<CoinFormat /> renders with 40 silver only", () => {
+    const tree = renderer
+        .create(<CoinFormat coins={4000} stripEmpty={true} />)
+        .toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
