98d2d6546d0eff4d723f2db2d7fc05752ef0f5f9
kwokkan <kwokkan@users.noreply.github.com>
2019-11-29 23:39:33 +0000

    #228: Convert stats widgets to function components.

--- apps/CardHero.NetCoreApp.TypeScript/ClientApp/components/card/CardSecondaryStatsGraphWidget.tsx
+++ apps/CardHero.NetCoreApp.TypeScript/ClientApp/components/card/CardSecondaryStatsGraphWidget.tsx
@@ -1,4 +1,4 @@
-﻿import React, { Component } from "react";
+﻿import React from "react";
 import { Radar } from "react-chartjs-2";
 import { ICardModel } from "../../clients/clients";
 
@@ -6,57 +6,51 @@ interface ICardSecondaryStatsGraphWidgetProps {
     card: ICardModel;
 }
 
-export class CardSecondaryStatsGraphWidget extends Component<ICardSecondaryStatsGraphWidgetProps, {}> {
-    constructor(props) {
-        super(props);
-    }
+export function CardSecondaryStatsGraphWidget(props: ICardSecondaryStatsGraphWidgetProps): JSX.Element {
+    const c = props.card;
 
-    render() {
-        const c = this.props.card;
-
-        const data: Chart.ChartData = {
-            labels: [
-                "Up Attack",
-                "Right Attack",
-                "Down Attack",
-                "Left Attack"
-            ],
-            datasets: [
-                {
-                    label: "Stats",
-                    backgroundColor: "rgba(0, 0, 200, 0.3)",
-                    borderColor: "rgba(0, 0, 200, 0.5)",
-                    pointBorderColor: "rgba(0, 0, 0, 0)",
-                    pointHoverBackgroundColor: "rgba(0, 0, 200, 0.5)",
-                    pointHoverBorderColor: "rgba(0, 0, 200, 0.5)",
-                    data: [
-                        c.upAttack,
-                        c.rightAttack,
-                        c.downAttack,
-                        c.leftAttack
-                    ]
-                }
-            ]
-        };
-
-        const options: Chart.ChartOptions = {
-            scale: {
-                ticks: {
-                    beginAtZero: true,
-                    max: 10
-                }
+    const data: Chart.ChartData = {
+        labels: [
+            "Up Attack",
+            "Right Attack",
+            "Down Attack",
+            "Left Attack"
+        ],
+        datasets: [
+            {
+                label: "Stats",
+                backgroundColor: "rgba(0, 0, 200, 0.3)",
+                borderColor: "rgba(0, 0, 200, 0.5)",
+                pointBorderColor: "rgba(0, 0, 0, 0)",
+                pointHoverBackgroundColor: "rgba(0, 0, 200, 0.5)",
+                pointHoverBorderColor: "rgba(0, 0, 200, 0.5)",
+                data: [
+                    c.upAttack,
+                    c.rightAttack,
+                    c.downAttack,
+                    c.leftAttack
+                ]
             }
-        };
+        ]
+    };
 
-        return (
-            <div className="card">
-                <h4 className="card-header">
-                    {c.name}
-                </h4>
-                <div className="card-body">
-                    <Radar data={data} options={options} />
-                </div>
+    const options: Chart.ChartOptions = {
+        scale: {
+            ticks: {
+                beginAtZero: true,
+                max: 10
+            }
+        }
+    };
+
+    return (
+        <div className="card">
+            <h4 className="card-header">
+                {c.name}
+            </h4>
+            <div className="card-body">
+                <Radar data={data} options={options} />
             </div>
-        );
-    }
+        </div>
+    );
 }
--- apps/CardHero.NetCoreApp.TypeScript/ClientApp/components/card/CardStatsGraphWidget.tsx
+++ apps/CardHero.NetCoreApp.TypeScript/ClientApp/components/card/CardStatsGraphWidget.tsx
@@ -1,4 +1,4 @@
-﻿import React, { Component } from "react";
+﻿import React from "react";
 import { Radar } from "react-chartjs-2";
 import { ICardModel } from "../../clients/clients";
 
@@ -6,55 +6,49 @@ interface ICardStatsGraphWidgetProps {
     card: ICardModel;
 }
 
-export class CardStatsGraphWidget extends Component<ICardStatsGraphWidgetProps, {}> {
-    constructor(props) {
-        super(props);
-    }
+export function CardStatsGraphWidget(props: ICardStatsGraphWidgetProps): JSX.Element {
+    const c = props.card;
 
-    render() {
-        const c = this.props.card;
-
-        const data: Chart.ChartData = {
-            labels: [
-                "Health",
-                "Attack",
-                "Defense"
-            ],
-            datasets: [
-                {
-                    label: "Stats",
-                    backgroundColor: "rgba(0, 0, 200, 0.3)",
-                    borderColor: "rgba(0, 0, 200, 0.5)",
-                    pointBorderColor: "rgba(0, 0, 0, 0)",
-                    pointHoverBackgroundColor: "rgba(0, 0, 200, 0.5)",
-                    pointHoverBorderColor: "rgba(0, 0, 200, 0.5)",
-                    data: [
-                        c.health,
-                        c.attack,
-                        c.defence
-                    ]
-                }
-            ]
-        };
-
-        const options: Chart.ChartOptions = {
-            scale: {
-                ticks: {
-                    beginAtZero: true,
-                    max: 10
-                }
+    const data: Chart.ChartData = {
+        labels: [
+            "Health",
+            "Attack",
+            "Defense"
+        ],
+        datasets: [
+            {
+                label: "Stats",
+                backgroundColor: "rgba(0, 0, 200, 0.3)",
+                borderColor: "rgba(0, 0, 200, 0.5)",
+                pointBorderColor: "rgba(0, 0, 0, 0)",
+                pointHoverBackgroundColor: "rgba(0, 0, 200, 0.5)",
+                pointHoverBorderColor: "rgba(0, 0, 200, 0.5)",
+                data: [
+                    c.health,
+                    c.attack,
+                    c.defence
+                ]
             }
-        };
+        ]
+    };
 
-        return (
-            <div className="card">
-                <h4 className="card-header">
-                    {c.name}
-                </h4>
-                <div className="card-body">
-                    <Radar data={data} options={options} />
-                </div>
+    const options: Chart.ChartOptions = {
+        scale: {
+            ticks: {
+                beginAtZero: true,
+                max: 10
+            }
+        }
+    };
+
+    return (
+        <div className="card">
+            <h4 className="card-header">
+                {c.name}
+            </h4>
+            <div className="card-body">
+                <Radar data={data} options={options} />
             </div>
-        );
-    }
+        </div>
+    );
 }
