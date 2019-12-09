6dd1cc8e05dc1760f6da832fb12049d3fbf21abc
kwokkan <kwokkan@users.noreply.github.com>
2019-12-09 00:53:21 +0000

    Add tests for Home component.

--- apps/CardHero.NetCoreApp.TypeScript/ClientApp/components/home/Home.tsx
+++ apps/CardHero.NetCoreApp.TypeScript/ClientApp/components/home/Home.tsx
@@ -1,25 +1,33 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { AppBootstrap } from '../shared/AppBootstrap';
+import { getRoutePrefix } from '../../utils/route';
 
-export function Home() {
-    const l = AppBootstrap.url;
+interface IHomeProps {
+    appName: string;
+    routePrefix?: string;
+}
+
+export function Home(props: IHomeProps) {
+    const appName = props.appName;
+    const routePrefix = getRoutePrefix(props.routePrefix);
+
     return (
         <Fragment>
             <div className="jumbotron">
-                <h1 className="display-3">{Constants.AppName}</h1>
+                <h1 className="display-3">{appName}</h1>
 
                 <p className="lead">
-                    {Constants.AppName} is a dynamic action game.
-                    </p>
+                    {appName} is a dynamic action game.
+                </p>
             </div>
+
             <div className="row">
                 <div className="col-lg-4">
                     <div className="card text-center">
                         <div className="card-body">
                             <h4 className="card-title">Cards</h4>
                             <p className="card-text">Discover over 1000 cards.</p>
-                            <Link className="btn btn-primary" to={l("Card")}>View Cards</Link>
+                            <Link className="btn btn-primary" to={`${routePrefix}Card`}>View Cards</Link>
                         </div>
                     </div>
                 </div>
@@ -29,7 +37,7 @@ export function Home() {
                         <div className="card-body">
                             <h4 className="card-title">Decks</h4>
                             <p className="card-text">Create you own decks.</p>
-                            <Link className="btn btn-primary" to={l("Deck")}>View Decks</Link>
+                            <Link className="btn btn-primary" to={`${routePrefix}Deck`}>View Decks</Link>
                         </div>
                     </div>
                 </div>
@@ -39,7 +47,7 @@ export function Home() {
                         <div className="card-body">
                             <h4 className="card-title">Battle</h4>
                             <p className="card-text">Battle against other players.</p>
-                            <Link className="btn btn-primary" to={l("Game")}>View Games</Link>
+                            <Link className="btn btn-primary" to={`${routePrefix}Game`}>View Games</Link>
                         </div>
                     </div>
                 </div>
--- apps/CardHero.NetCoreApp.TypeScript/ClientApp/components/home/HomeApp.tsx
+++ apps/CardHero.NetCoreApp.TypeScript/ClientApp/components/home/HomeApp.tsx
@@ -1,12 +1,15 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { Home } from './Home';
 
-export class HomeApp extends PureComponent {
-    render() {
-        return (
-            <div className="col-lg-12">
-                <Home />
-            </div>
-        );
-    }
+interface IHomeAppProps {
+    appName: string;
+    routePrefix?: string;
+}
+
+export function HomeApp(props: IHomeAppProps) {
+    return (
+        <div className="col-lg-12">
+            <Home appName={props.appName} routePrefix={props.routePrefix} />
+        </div>
+    );
 }
