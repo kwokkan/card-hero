a5270b28e4b5a212fdc6c0fc55953a17acf7eb3f
kwokkan <kwokkan@users.noreply.github.com>
2019-12-09 21:45:56 +0000

    Add basic integration tests for dotnet.

---
+++ tests/CardHero.NetCoreApp.IntegrationTests/AccountApiControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+using CardHero.NetCoreApp.TypeScript;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using Xunit;
+
+namespace CardHero.NetCoreApp.IntegrationTests
+{
+    public class AccountApiControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public AccountApiControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetAsync_WithoutLogin_ReturnsUnauthorized()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("api/account");
+
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+    }
+}
