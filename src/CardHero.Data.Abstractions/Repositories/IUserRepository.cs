f381c7067ac9f89fc53387c94a7132cdd24389d0
kwokkan <kwokkan@users.noreply.github.com>
2019-12-11 00:10:12 +0000

    #97: Use a user repository for managing updates to user.

--- src/CardHero.Data.Abstractions/Repositories/IUserRepository.cs
+++ src/CardHero.Data.Abstractions/Repositories/IUserRepository.cs
@@ -7,6 +7,10 @@ namespace CardHero.Data.Abstractions
     {
         Task<UserData> CreateUserAsync(string identifier, string idp, string name, long coins, CancellationToken cancellationToken = default);
 
-        Task<UserData> GetUserByIdentifier(string identifier, string idp, CancellationToken cancellationToken = default);
+        Task<UserData> GetUserByIdAsync(int id, CancellationToken cancellationToken = default);
+
+        Task<UserData> GetUserByIdentifierAsync(string identifier, string idp, CancellationToken cancellationToken = default);
+
+        Task UpdateUserAsync(int id, UserUpdateData update, CancellationToken cancellationToken = default);
     }
 }
