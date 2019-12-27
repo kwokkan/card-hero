d0f2a2d24f3655a51aeb5aa49cc704a21987bb4a
kwokkan <kwokkan@users.noreply.github.com>
2019-12-27 23:23:37 +0000

    Remove explicit usages to Newtonsoft and use System.Text.Json instead.

--- apps/CardHero.NetCoreApp.TypeScript/Extensions/GitHubAuthenticationBuilderExtensions.cs
+++ apps/CardHero.NetCoreApp.TypeScript/Extensions/GitHubAuthenticationBuilderExtensions.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Security.Claims;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 using CardHero.NetCoreApp.TypeScript;
@@ -10,12 +11,83 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.Extensions.DependencyInjection;
 
-using Newtonsoft.Json.Linq;
-
 namespace Microsoft.AspNetCore.Builder
 {
     public static class GitHubAuthenticationBuilderExtensions
     {
+#pragma warning disable CA1812
+        private class GithubUserInfo
+        {
+            public int? Id { get; set; }
+            public string Login { get; set; }
+            public string Name { get; set; }
+        }
+#pragma warning restore CA1812
+
+        private static readonly JsonSerializerOptions _options = new JsonSerializerOptions
+        {
+            AllowTrailingCommas = true,
+            IgnoreNullValues = true,
+            PropertyNameCaseInsensitive = true,
+            ReadCommentHandling = JsonCommentHandling.Skip,
+        };
+
+        private static OAuthEvents GetOAuthEvents(string userInformationEndpoint)
+        {
+            return new OAuthEvents
+            {
+                OnCreatingTicket = async context =>
+                {
+                    using (var request = new HttpRequestMessage(HttpMethod.Get, userInformationEndpoint))
+                    {
+                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+                        var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+
+                        if (!response.IsSuccessStatusCode)
+                        {
+                            return;
+                        }
+
+                        try
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+
+                            var payload = JsonSerializer.Deserialize<GithubUserInfo>(content, _options);
+
+                            var githubId = payload.Id;
+
+                            if (githubId.HasValue && githubId > 0)
+                            {
+                                var nameValue = payload.Name ?? payload.Login ?? "GitHub User #" + githubId;
+                                context.Identity.AddClaim(new Claim("sub", githubId.ToString()));
+                                context.Identity.AddClaim(new Claim("idp", GitHubAuthenticationOptions.DefaultAuthenticationScheme));
+                                context.Identity.AddClaim(new Claim("name", nameValue));
+                            }
+                        }
+                        catch
+                        {
+                            return;
+                        }
+                    }
+                },
+                OnRedirectToAuthorizationEndpoint = context =>
+                {
+                    if (context.Request.Path.StartsWithSegments("/api"))
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    }
+                    else
+                    {
+                        context.Response.Redirect(context.RedirectUri);
+                    }
+
+                    return Task.CompletedTask;
+                },
+            };
+        }
+
         public static AuthenticationBuilder AddGitHubAuthentication(this AuthenticationBuilder authenticationBuilder, GitHubAuthenticationOptions options)
         {
             authenticationBuilder.AddOAuth(GitHubAuthenticationOptions.DefaultAuthenticationScheme, GitHubAuthenticationOptions.DefaultDisplayName, x =>
@@ -31,50 +103,7 @@ namespace Microsoft.AspNetCore.Builder
 
                 x.ClaimsIssuer = GitHubAuthenticationOptions.DefaultClaimsIssuer;
 
-                x.Events = new OAuthEvents
-                {
-                    OnCreatingTicket = async context =>
-                    {
-                        using (var request = new HttpRequestMessage(HttpMethod.Get, options.UserInformationEndpoint))
-                        {
-                            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
-
-                            var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
-
-                            if (!response.IsSuccessStatusCode)
-                            {
-                                return;
-                            }
-
-                            var content = await response.Content.ReadAsStringAsync();
-                            var payload = JObject.Parse(content);
-
-                            var githubId = payload.GetValue("id")?.ToObject<int>();
-
-                            if (githubId.HasValue && githubId > 0)
-                            {
-                                var nameValue = payload.GetValue("name")?.ToObject<string>() ?? payload.GetValue("login")?.ToObject<string>() ?? "GitHub User #" + githubId;
-                                context.Identity.AddClaim(new Claim("sub", githubId.ToString()));
-                                context.Identity.AddClaim(new Claim("idp", GitHubAuthenticationOptions.DefaultAuthenticationScheme));
-                                context.Identity.AddClaim(new Claim("name", nameValue));
-                            }
-                        }
-                    },
-                    OnRedirectToAuthorizationEndpoint = context =>
-                    {
-                        if (context.Request.Path.StartsWithSegments("/api"))
-                        {
-                            context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-                        }
-                        else
-                        {
-                            context.Response.Redirect(context.RedirectUri);
-                        }
-
-                        return Task.CompletedTask;
-                    },
-                };
+                x.Events = GetOAuthEvents(options.UserInformationEndpoint);
 
                 if (options.Scopes != null)
                 {
