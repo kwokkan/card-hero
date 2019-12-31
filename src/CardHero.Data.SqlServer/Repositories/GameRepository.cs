1d779fbf83d2e097890dba3d8d0087a19d72ab49
kwokkan <kwokkan@users.noreply.github.com>
2019-12-31 17:10:12 +0000

    #507: Cleanup packages for EF Core SQL Server and use DbContext directly.

--- src/CardHero.Data.SqlServer/Repositories/GameRepository.cs
+++ src/CardHero.Data.SqlServer/Repositories/GameRepository.cs
@@ -14,55 +14,53 @@ namespace CardHero.Data.SqlServer
     {
         private const int DefaultPageSize = 30;
 
-        private readonly ICardHeroDataDbContextFactory _factory;
+        private readonly CardHeroDataDbContext _context;
         private readonly IMapper<Game, GameData> _gameMapper;
         private readonly IMapper<GameUser, GameUserData> _gameUserMapper;
 
         public GameRepository(
-            ICardHeroDataDbContextFactory factory,
+            CardHeroDataDbContext context,
             IMapper<Game, GameData> gameMapper,
             IMapper<GameUser, GameUserData> gameUseMapper
         )
         {
-            _factory = factory;
+            _context = context;
+
             _gameMapper = gameMapper;
             _gameUserMapper = gameUseMapper;
         }
 
         private async Task<SearchResult<GameData>> FindGamesInternalAsync(GameSearchFilter filter, CancellationToken cancellationToken)
         {
-            using (var context = _factory.Create())
+            var query = _context.Game.AsQueryable();
+
+            if (filter.GameId.HasValue)
             {
-                var query = context.Game.AsQueryable();
-
-                if (filter.GameId.HasValue)
-                {
-                    query = query.Where(x => x.GamePk == filter.GameId.Value);
-                }
-
-                if (filter.Type.HasValue)
-                {
-                    query = query.Where(x => x.GameTypeFk == (int)filter.Type.Value);
-                }
-
-                var totalCount = await query.CountAsync(cancellationToken: cancellationToken);
-
-                query = query.OrderByDescending(x => x.GamePk);
-
-                query = query.Skip(0).Take(DefaultPageSize);
-
-                var results = query.Select(_gameMapper.Map).ToArray();
-
-                var result = new SearchResult<GameData>
-                {
-                    CurrentPage = 0,
-                    PageSize = DefaultPageSize,
-                    Results = results,
-                    TotalCount = totalCount,
-                };
-
-                return result;
+                query = query.Where(x => x.GamePk == filter.GameId.Value);
             }
+
+            if (filter.Type.HasValue)
+            {
+                query = query.Where(x => x.GameTypeFk == (int)filter.Type.Value);
+            }
+
+            var totalCount = await query.CountAsync(cancellationToken: cancellationToken);
+
+            query = query.OrderByDescending(x => x.GamePk);
+
+            query = query.Skip(0).Take(DefaultPageSize);
+
+            var results = query.Select(_gameMapper.Map).ToArray();
+
+            var result = new SearchResult<GameData>
+            {
+                CurrentPage = 0,
+                PageSize = DefaultPageSize,
+                Results = results,
+                TotalCount = totalCount,
+            };
+
+            return result;
         }
 
         private async Task<GameData> GetGameByIdInternalAsync(int id, CancellationToken cancellationToken)
@@ -87,12 +85,9 @@ namespace CardHero.Data.SqlServer
                 StartTime = DateTime.UtcNow,
             };
 
-            using (var context = _factory.Create(trackChanges: true))
-            {
-                context.Game.Add(data);
+            _context.Game.Add(data);
 
-                await context.SaveChangesAsync(cancellationToken: cancellationToken);
-            }
+            await _context.SaveChangesAsync(cancellationToken: cancellationToken);
 
             return await GetGameByIdInternalAsync(data.GamePk, cancellationToken: cancellationToken);
         }
@@ -109,63 +104,54 @@ namespace CardHero.Data.SqlServer
 
         async Task<GameUserData[]> IGameRepository.GetGameUsersAsync(int gameId, CancellationToken cancellationToken)
         {
-            using (var context = _factory.Create())
-            {
-                var result = await context
-                    .GameUser
-                    .Where(x => x.GameFk == gameId)
-                    .Select(x => _gameUserMapper.Map(x))
-                    .ToArrayAsync(cancellationToken: cancellationToken);
+            var result = await _context
+                .GameUser
+                .Where(x => x.GameFk == gameId)
+                .Select(x => _gameUserMapper.Map(x))
+                .ToArrayAsync(cancellationToken: cancellationToken);
 
-                return result;
-            }
+            return result;
         }
 
         async Task<MoveData[]> IGameRepository.GetMovesByGameIdAsync(int gameId, CancellationToken cancellationToken)
         {
-            using (var context = _factory.Create())
-            {
-                var result = await context
-                    .Move
-                    .Include(x => x.TurnFkNavigation)
-                    .Where(x => x.TurnFkNavigation.GameFk == gameId)
-                    .Select(x => new MoveData
-                    {
-                        GameDeckCardCollectionId = x.GameDeckCardCollectionFk,
-                        Column = x.Column,
-                        GameId = x.TurnFkNavigation.GameFk,
-                        Row = x.Row,
-                        GameUserId = x.TurnFkNavigation.CurrentGameUserFk,
-                    })
-                    .ToArrayAsync(cancellationToken: cancellationToken);
+            var result = await _context
+                .Move
+                .Include(x => x.TurnFkNavigation)
+                .Where(x => x.TurnFkNavigation.GameFk == gameId)
+                .Select(x => new MoveData
+                {
+                    GameDeckCardCollectionId = x.GameDeckCardCollectionFk,
+                    Column = x.Column,
+                    GameId = x.TurnFkNavigation.GameFk,
+                    Row = x.Row,
+                    GameUserId = x.TurnFkNavigation.CurrentGameUserFk,
+                })
+                .ToArrayAsync(cancellationToken: cancellationToken);
 
-                return result;
-            }
+            return result;
         }
 
         async Task<GameData> IGameRepository.UpdateGameAsync(int id, GameUpdateData update, CancellationToken cancellationToken)
         {
-            using (var context = _factory.Create(trackChanges: true))
+            var existingGame = await _context.Game.SingleOrDefaultAsync(x => x.GamePk == id, cancellationToken: cancellationToken);
+
+            if (existingGame == null)
             {
-                var existingGame = await context.Game.SingleOrDefaultAsync(x => x.GamePk == id, cancellationToken: cancellationToken);
-
-                if (existingGame == null)
-                {
-                    throw new CardHeroDataException($"Game { id } does not exist.");
-                }
-
-                if (update.CurrentGameUserId.IsSet)
-                {
-                    existingGame.CurrentGameUserFk = update.CurrentGameUserId.Value;
-                }
-
-                if (update.WinnerId.IsSet)
-                {
-                    existingGame.WinnerFk = update.WinnerId.Value;
-                }
-
-                await context.SaveChangesAsync(cancellationToken: cancellationToken);
+                throw new CardHeroDataException($"Game { id } does not exist.");
             }
+
+            if (update.CurrentGameUserId.IsSet)
+            {
+                existingGame.CurrentGameUserFk = update.CurrentGameUserId.Value;
+            }
+
+            if (update.WinnerId.IsSet)
+            {
+                existingGame.WinnerFk = update.WinnerId.Value;
+            }
+
+            await _context.SaveChangesAsync(cancellationToken: cancellationToken);
 
             return await GetGameByIdInternalAsync(id, cancellationToken: cancellationToken);
         }
