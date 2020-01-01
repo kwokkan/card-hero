f4756541785588304176c307172790aa68029238
kwokkan <kwokkan@users.noreply.github.com>
2020-01-01 23:16:52 +0000

    #507: Use Postgres for integration tests.

--- tests/CardHero.NetCoreApp.IntegrationTests/Helpers/CustomWebApplicationFactory.cs
+++ tests/CardHero.NetCoreApp.IntegrationTests/Helpers/CustomWebApplicationFactory.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Linq;
 
-using CardHero.Data.SqlServer.EntityFramework;
+using CardHero.Data.PostgreSql.EntityFramework;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
