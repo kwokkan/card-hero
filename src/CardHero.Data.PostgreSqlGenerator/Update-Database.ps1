3c54360a396c491b3d68c12b78d1cb0139113758
kwokkan <kwokkan@users.noreply.github.com>
2020-01-01 22:45:26 +0000

    #507: Initial PostgreSQL generation project setup.

---
+++ src/CardHero.Data.PostgreSqlGenerator/Update-Database.ps1
@@ -0,0 +1,24 @@
+﻿Scaffold-DbContext `
+"Host=localhost;Database=cardhero;Username=cardhero_user;Password=password" `
+Npgsql.EntityFrameworkCore.PostgreSQL `
+-Force `
+-Project CardHero.Data.PostgreSql `
+-StartupProject CardHero.Data.PostgreSqlGenerator `
+-Context CardHeroDataDbContext `
+-OutputDir EntityFramework `
+-Tables `
+Card,`
+CardCollection,`
+CardFavourite,`
+Deck,`
+DeckCardCollection,`
+DeckFavourite,`
+Game,`
+GameDeck,`
+GameDeckCardCollection,`
+GameUser,`
+Move,`
+Rarity,`
+StoreItem,`
+Turn,`
+User
