f3befe054277c81135bdb7b4ee74e979ee0953a2
kwokkan <kwokkan@users.noreply.github.com>
2020-01-04 23:57:21 +0000

    #230: Add card pack support to SQL Server db.

---
+++ databases/CardHero.Database.SqlServer/Scripts/PostDeployment/Data/CardPack.sql
@@ -0,0 +1,28 @@
+﻿print 'CardPack.sql'
+set identity_insert dbo.CardPack on
+
+merge into dbo.CardPack as Target
+using (values
+	(1, 'Standard', 'Standard cards.')
+) as Source (
+	CardPack_PK,
+	Name,
+	Description
+)
+on
+	Target.CardPack_PK = Source.CardPack_PK
+
+when matched then
+	update set
+		Target.Name = Source.Name,
+		Target.Description = Source.Description
+
+when not matched by Source then
+	delete
+
+when not matched by Target then
+	insert (CardPack_PK, Name, Description)
+	values (Source.CardPack_PK, Source.Name, Source.Description)
+;
+
+set identity_insert dbo.CardPack off
