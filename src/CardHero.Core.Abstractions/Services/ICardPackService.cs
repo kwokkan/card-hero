63862dffc8f1559fe9f5e4fd3d50693053ce3dbc
kwokkan <kwokkan@users.noreply.github.com>
2020-01-04 22:23:53 +0000

    #230: Add card pack methods to API.

---
+++ src/CardHero.Core.Abstractions/Services/ICardPackService.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using CardHero.Core.Models;
+
+namespace CardHero.Core.Abstractions
+{
+    /// <summary>
+    /// Defines methods to getting card packs.
+    /// </summary>
+    public interface ICardPackService
+    {
+        /// <summary>
+        /// Gets a list of card packs.
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>A list of card packs.</returns>
+        Task<SearchResult<CardPackModel>> GetCardPacksAsync(CancellationToken cancellationToken = default);
+    }
+}
