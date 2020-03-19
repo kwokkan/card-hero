ab10ba9e1f01dee495bf23001a7dda3d44c1851c
kwokkan <kwokkan@users.noreply.github.com>
2020-03-19 19:28:02 +0000

    #547: Add tests for joining a game.

--- tests/CardHero.NetCoreApp.IntegrationTests/IntegrationTestBase.cs
+++ tests/CardHero.NetCoreApp.IntegrationTests/IntegrationTestBase.cs
@@ -11,6 +11,8 @@ namespace CardHero.NetCoreApp.IntegrationTests
     public abstract class IntegrationTestBase
     {
         protected const int IdCard = 100;
+        protected const int IdDeck = 400;
+        protected const int IdGame = 700;
         protected const int IdCardCollection = 900;
 
         private readonly List<BaseWebApplicationFactory> _factories = new List<BaseWebApplicationFactory>();
