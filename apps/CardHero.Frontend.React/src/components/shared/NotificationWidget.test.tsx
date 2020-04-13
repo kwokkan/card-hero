02341b3ec413a2bc032cb3f77b168019e0e39544
kwokkan <kwokkan@users.noreply.github.com>
2020-04-13 21:44:02 +0100

    #18: Initial work to get static notifications working. (#982)
    * #18: Initial work to get static notifications working.
    
    * #18: Improve styling of notifications.
    
    * #18: Allow notifications to be dismissable.
    
    * #18: Add tests for notifications.
    
    * #18: Fix warning with extra brackets.

---
+++ apps/CardHero.Frontend.React/src/components/shared/NotificationWidget.test.tsx
@@ -0,0 +1,43 @@
+﻿import React from "react";
+import renderer from "react-test-renderer";
+import { INotificationContextProviderProps, NotificationContextProvider } from "../../contexts/NotificationContextProvider";
+import { NotificationType } from "../../types/NotificationType";
+import { NotificationWidget } from "./NotificationWidget";
+
+test("<NotificationWidget /> renders with NotificationContextProvider and null notifications", () => {
+    const tree = renderer
+        .create(
+            <NotificationContextProvider value={null}>
+                <NotificationWidget />
+            </NotificationContextProvider>
+        )
+        .toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
+
+test("<NotificationWidget /> renders with NotificationContextProvider and default notifications", () => {
+    const defaultProps: INotificationContextProviderProps = {
+        notifications: [
+            {
+                message: "Test message",
+                title: "Test title",
+                type: NotificationType.Info
+            },
+            {
+                message: "Danger message",
+                title: "Danger title",
+                type: NotificationType.Danger
+            }
+        ]
+    };
+    const tree = renderer
+        .create(
+            <NotificationContextProvider value={defaultProps}>
+                <NotificationWidget />
+            </NotificationContextProvider>
+        )
+        .toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
