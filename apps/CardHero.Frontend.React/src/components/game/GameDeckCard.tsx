c58f0012fc9ddb537a625084d65ee18aab96c00f
kwokkan <kwokkan@users.noreply.github.com>
2021-11-03 15:53:25 +0000

    Fix build error with react-dnd upgrade.

--- apps/CardHero.Frontend.React/src/components/game/GameDeckCard.tsx
+++ apps/CardHero.Frontend.React/src/components/game/GameDeckCard.tsx
@@ -14,6 +14,7 @@ export function GameDeckCard(props: IGameDeckCardProps) {
     const isUsable = true;
 
     const [{ isDragging }, drag] = useDrag({
+        type: DragType.Card.toString(),
         item: {
             type: DragType.Card.toString(),
             gameDeckCardCollectionId: dc.id
