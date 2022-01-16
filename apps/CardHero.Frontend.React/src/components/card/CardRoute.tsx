7bc42ef6e92e0ca793735f5dea6ca6e0d4085874
kwokkan <kwokkan@users.noreply.github.com>
2022-01-16 17:02:44 +0000

    Migrate react-router v5 routes to v6.

---
+++ apps/CardHero.Frontend.React/src/components/card/CardRoute.tsx
@@ -0,0 +1,14 @@
+﻿import React from "react";
+import { useParams } from "react-router-dom";
+import { Card } from "./Card";
+
+export function CardRoute(): JSX.Element | null {
+    const params = useParams();
+    const id = params.id as unknown as number;
+
+    return (
+        <Card
+            id={id}
+        />
+    );
+}
